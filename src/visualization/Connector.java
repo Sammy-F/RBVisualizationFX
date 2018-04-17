9ab7ccd35c121134dd22d1dcd075e66e515ce2db
kgurgel <kgurgel@macalester.edu>
2018-04-17 00:53:15 -0500

    added tiny change; mostly just a tiny experiment

--- src/visualization/Connector.java
+++ src/visualization/Connector.java
@@ -26,14 +26,14 @@ public class Connector extends Line {
         Insets parentInsets = parentNode.getInsets();
 
         if (childNode.getLayoutX() < parentNode.getLayoutX()) {
-            setStartX(childInsets.getLeft() + childNode.getRadius());  //did not fix these since this never happens yet
+            setStartX(childInsets.getLeft() + childNode.getRadius());  // did not fix these since this never happens yet
             setEndX(parentInsets.getLeft() + parentNode.getRadius());
 
             setStartY(childInsets.getTop());
             setEndY(parentInsets.getTop() + parentNode.getRadius()*2);
         }
         else {
-            setStartX(parentInsets.getLeft() + parentNode.getRadius()*2);  //these look slightly prettier now
+            setStartX(parentInsets.getLeft() + parentNode.getRadius()*2);  // these look slightly prettier now
             setEndX(childInsets.getLeft() + childNode.getRadius()); //(also yeah, this change is tiny, I will do more!)
 
             setStartY(parentInsets.getTop() + parentNode.getRadius()*2);
