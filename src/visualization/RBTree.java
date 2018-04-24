8bcff4c1f905bf9b5e9437e0ac3843c2c8795a84
Sammy-F <sfritsch@macalester.edu>
2018-04-24 14:35:11 -0500

    added some skeletal code to RBTree

--- src/visualization/RBTree.java
+++ src/visualization/RBTree.java
@@ -6,39 +6,34 @@ package visualization;
 public class RBTree {
 
     private Node root;
-    private RBTree lastTree;
 
     public RBTree() {
 
+    }
+
+    public RBTree returnCopy() {
+        //TODO: return an actual copy of this tree
+        return this;
     }
 
     /**
      * Handle logic for node insertion and return the new tree
      * @return
      */
-    public RBTree insertNode() {
-        RBTree newTree = new RBTree();
-        newTree.setLastTree(this);
-
-        return newTree;
+    public void insertNode(double value) {
+        //TODO
     }
 
     /**
      * Handle logic for node deletion and return the new tree
      * @return
      */
-    public RBTree deleteNode() {
-        RBTree newTree = new RBTree();
-        newTree.setLastTree(this);
-
-        return newTree;
+    public void deleteNode(double value) {
+        //TODO
 
     }
 
     public void setRoot(Node newRoot) { root = newRoot; }
     public Node getRoot() { return root; }
-
-    public void setLastTree(RBTree newLastTree) { lastTree = newLastTree; }
-    public RBTree lastTree() { return lastTree; }
 
 }
